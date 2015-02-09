961274f058f5087be7e096f3d3f4889b609ea133
Ed Shadi <edshadi@gmail.com>
2015-02-09 11:20:44 -0800

    server accepts cross origin requests

--- server/index.js
+++ server/index.js
@@ -4,6 +4,12 @@ var Firebase = require('firebase')
 var fb = new Firebase('https://backbonereact.firebaseio.com/')
 var bodyParser = require('body-parser')
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(bodyParser());
 
 app.set('view engine', 'jade');
